caa3bd903dc53c7a989625286728fa483cccbe85
Brian Hogan <brianhogan@napcs.com>
2011-11-05 20:52:29 -0500

    respond_to changes

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,5 +1,12 @@
 class NewsController < ApplicationController
   def index
     @news_items = NewsItem.order("created_at desc").page(params[:page])
+
+    respond_to do |format|
+      format.html #do nothing.
+      format.json { render :json => @news_items.to_json }
+      format.xml { render :xml => @news_items.to_xml }
+    end
+
   end
 end
